e822c791cd3d0295ef7e9b428d328e7d7cc16d20
priyanshu chouhan <125085399+priyanshuchouhan@users.noreply.github.com>
2026-06-22 22:16:51 +0530

    Create security.tf

---
+++ terraform/security.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "ecs_sg" {
+  name = "todo-ecs-sg"
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
